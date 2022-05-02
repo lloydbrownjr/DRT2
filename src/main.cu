402807f9676c7f397f358ac26b03764dd318b466
lab352 <lloydbrown@berkeley.edu>
2022-05-02 18:03:04 -0400

    add options and start of benchmarking code

--- src/main.cu
+++ src/main.cu
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <time.h>
 #include <float.h>
 #include <curand_kernel.h>
@@ -8,20 +9,11 @@
 #include "hitable_list.h"
 #include "camera.h"
 #include "material.h"
+#include "cuda_errors.h"
+#include "../common/options.h"
 
-// limited version of checkCudaErrors from helper_cuda.h in CUDA examples
-#define checkCudaErrors(val) check_cuda( (val), #val, __FILE__, __LINE__ )
-
-void check_cuda(cudaError_t result, char const *const func, const char *const file, int const line) {
-    if (result) {
-        std::cerr << "CUDA error = " << static_cast<unsigned int>(result) << " at " <<
-            file << ":" << line << " '" << func << "' \n";
-        // Make sure we call CUDA Device Reset before exiting
-        cudaDeviceReset();
-        exit(99);
-    }
-}
-
+// This method determines the color of a ray going through the scene by tracing it through the scene and hitting objects.
+// It has been modified to use CUDA as described below.
 // Matching the C++ code would recurse enough into color() calls that
 // it was blowing up the stack, so we have to turn this into a
 // limited-depth loop instead.  Later code in the book limits to a max
@@ -70,7 +62,7 @@ __global__ void render_init(int max_x, int max_y, curandState *rand_state) {
     curand_init(1984+pixel_index, 0, 0, &rand_state[pixel_index]);
 }
 
-__global__ void render(vec3 *fb, int max_x, int max_y, int ns, camera **cam, hitable **world, curandState *rand_state) {
+__global__ void render(vec3 *frame_buffer, int max_x, int max_y, int ns, camera **cam, hitable **world, curandState *rand_state) {
     int i = threadIdx.x + blockIdx.x * blockDim.x;
     int j = threadIdx.y + blockIdx.y * blockDim.y;
     if((i >= max_x) || (j >= max_y)) return;
@@ -88,7 +80,7 @@ __global__ void render(vec3 *fb, int max_x, int max_y, int ns, camera **cam, hit
     col[0] = sqrt(col[0]);
     col[1] = sqrt(col[1]);
     col[2] = sqrt(col[2]);
-    fb[pixel_index] = col;
+    frame_buffer[pixel_index] = col;
 }
 
 #define RND (curand_uniform(&local_rand_state))
@@ -136,6 +128,13 @@ __global__ void create_world(hitable **d_list, hitable **d_world, camera **d_cam
     }
 }
 
+// Moves the camera's origin to create a new scene
+__global__ void move_cam(camera **d_camera) {
+    if (threadIdx.x == 0 && blockIdx.x == 0) {
+        (*d_camera)->origin += vec3(0,0,-0.1);
+    }
+}
+
 __global__ void free_world(hitable **d_list, hitable **d_world, camera **d_camera) {
     for(int i=0; i < 22*22+1+3; i++) {
         delete ((sphere *)d_list[i])->mat_ptr;
@@ -145,12 +144,13 @@ __global__ void free_world(hitable **d_list, hitable **d_world, camera **d_camer
     delete *d_camera;
 }
 
-void write_fb(vec3 *fb, int nx, int ny, int max_x, int max_y) {
+// Writes the image to a ppm file.
+void write_frame_buffer(vec3 *frame_buffer, int nx, int ny, int max_x, int max_y) {
     FILE *f = fopen("output.ppm", "w");
     fprintf(f, "P3\n%d %d\n255\n", max_x, max_y);
     for(int j=0; j < max_y; j++) {
         for(int i=0; i < max_x; i++) {
-            vec3 col = fb[j*max_x + i];
+            vec3 col = frame_buffer[j*max_x + i];
             int ir = int(255.99*col[0]);
             int ig = int(255.99*col[1]);
             int ib = int(255.99*col[2]);
@@ -161,22 +161,20 @@ void write_fb(vec3 *fb, int nx, int ny, int max_x, int max_y) {
     fclose(f);
 }
 
-int main() {
-    int nx = 1200;
-    int ny = 800;
-    int ns = 10;
+// Renders a single image and writes it to a ppm file.
+void test_render(int image_height, int image_width, int samples_per_pixel) {
     int tx = 8;
     int ty = 8;
 
-    std::cerr << "Rendering a " << nx << "x" << ny << " image with " << ns << " samples per pixel ";
+    std::cerr << "Rendering a " << image_width << "x" << image_height << " image with " << samples_per_pixel << " samples per pixel ";
     std::cerr << "in " << tx << "x" << ty << " blocks.\n";
 
-    int num_pixels = nx*ny;
-    size_t fb_size = num_pixels*sizeof(vec3);
+    int num_pixels = image_width*image_height;
+    size_t frame_buffer_size = num_pixels*sizeof(vec3);
 
-    // allocate FB
-    vec3 *fb;
-    checkCudaErrors(cudaMallocManaged((void **)&fb, fb_size));
+    // allocate frame_buffer
+    vec3 *frame_buffer;
+    checkCudaErrors(cudaMallocManaged((void **)&frame_buffer, frame_buffer_size));
 
     // allocate random state
     curandState *d_rand_state;
@@ -197,27 +195,27 @@ int main() {
     checkCudaErrors(cudaMalloc((void **)&d_world, sizeof(hitable *)));
     camera **d_camera;
     checkCudaErrors(cudaMalloc((void **)&d_camera, sizeof(camera *)));
-    create_world<<<1,1>>>(d_list, d_world, d_camera, nx, ny, d_rand_state2);
+    create_world<<<1,1>>>(d_list, d_world, d_camera, image_width, image_height, d_rand_state2);
     checkCudaErrors(cudaGetLastError());
     checkCudaErrors(cudaDeviceSynchronize());
 
     clock_t start, stop;
     start = clock();
     // Render our buffer
-    dim3 blocks(nx/tx+1,ny/ty+1);
+    dim3 blocks(image_width/tx+1,image_height/ty+1);
     dim3 threads(tx,ty);
-    render_init<<<blocks, threads>>>(nx, ny, d_rand_state);
+    render_init<<<blocks, threads>>>(image_width, image_height, d_rand_state);
     checkCudaErrors(cudaGetLastError());
     checkCudaErrors(cudaDeviceSynchronize());
-    render<<<blocks, threads>>>(fb, nx, ny,  ns, d_camera, d_world, d_rand_state);
+    render<<<blocks, threads>>>(frame_buffer, image_width, image_height, samples_per_pixel, d_camera, d_world, d_rand_state);
     checkCudaErrors(cudaGetLastError());
     checkCudaErrors(cudaDeviceSynchronize());
     stop = clock();
     double timer_seconds = ((double)(stop - start)) / CLOCKS_PER_SEC;
     std::cerr << "took " << timer_seconds << " seconds.\n";
 
-    // Output FB as Image
-    write_fb(fb, nx, ny, nx, ny);
+    // Output frame_buffer as Image
+    write_frame_buffer(frame_buffer, image_width, image_height, image_width, image_height);
 
     // clean up
     checkCudaErrors(cudaDeviceSynchronize());
@@ -228,7 +226,137 @@ int main() {
     checkCudaErrors(cudaFree(d_list));
     checkCudaErrors(cudaFree(d_rand_state));
     checkCudaErrors(cudaFree(d_rand_state2));
-    checkCudaErrors(cudaFree(fb));
+    checkCudaErrors(cudaFree(frame_buffer));
 
     cudaDeviceReset();
+}
+
+void benchmark_single(int image_height, int image_width, int samples_per_pixel, int num_frames_to_render) {
+    int tx = 8;
+    int ty = 8;
+
+    std::cerr << "Benchmarking the rendering of " << image_width << "x" << image_height << " images with " << samples_per_pixel << " samples per pixel ";
+    std::cerr << "in " << tx << "x" << ty << " blocks.\n";
+
+    int num_pixels = image_width*image_height;
+    size_t frame_buffer_size = num_pixels*sizeof(vec3);
+
+    // allocate frame_buffer
+    vec3 *frame_buffer;
+    checkCudaErrors(cudaMallocManaged((void **)&frame_buffer, frame_buffer_size));
+
+    // allocate random state
+    curandState *d_rand_state;
+    checkCudaErrors(cudaMalloc((void **)&d_rand_state, num_pixels*sizeof(curandState)));
+    curandState *d_rand_state2;
+    checkCudaErrors(cudaMalloc((void **)&d_rand_state2, 1*sizeof(curandState)));
+
+    // we need that 2nd random state to be initialized for the world creation
+    rand_init<<<1,1>>>(d_rand_state2);
+    checkCudaErrors(cudaGetLastError());
+    checkCudaErrors(cudaDeviceSynchronize());
+
+    // make our world of hitables & the camera
+    hitable **d_list;
+    int num_hitables = 22*22+1+3;
+    checkCudaErrors(cudaMalloc((void **)&d_list, num_hitables*sizeof(hitable *)));
+    hitable **d_world;
+    checkCudaErrors(cudaMalloc((void **)&d_world, sizeof(hitable *)));
+    camera **d_camera;
+    checkCudaErrors(cudaMalloc((void **)&d_camera, sizeof(camera *)));
+    create_world<<<1,1>>>(d_list, d_world, d_camera, image_width, image_height, d_rand_state2);
+    checkCudaErrors(cudaGetLastError());
+    checkCudaErrors(cudaDeviceSynchronize());
+
+    clock_t start, stop;
+    start = clock();
+    // Render our buffer
+    dim3 blocks(image_width/tx+1,image_height/ty+1);
+    dim3 threads(tx,ty);
+    render_init<<<blocks, threads>>>(image_width, image_height, d_rand_state);
+    checkCudaErrors(cudaGetLastError());
+    checkCudaErrors(cudaDeviceSynchronize());
+    for (int i = 0; i < num_frames_to_render; i++) {
+        // Render the current frame and make sure it worked.
+        render<<<blocks, threads>>>(frame_buffer, image_width, image_height, samples_per_pixel, d_camera, d_world, d_rand_state);
+        checkCudaErrors(cudaGetLastError());
+        checkCudaErrors(cudaDeviceSynchronize());
+        // Move the camera to create the next frame.
+        move_cam<<<blocks, threads>>>(d_camera);
+    }
+    stop = clock();
+    double timer_seconds = ((double)(stop - start)) / CLOCKS_PER_SEC;
+    std::cerr << "took " << timer_seconds << " seconds.\n";
+
+    // Output frame_buffer as Image
+    write_frame_buffer(frame_buffer, image_width, image_height, image_width, image_height);
+
+    // clean up
+    checkCudaErrors(cudaDeviceSynchronize());
+    free_world<<<1,1>>>(d_list,d_world,d_camera);
+    checkCudaErrors(cudaGetLastError());
+    checkCudaErrors(cudaFree(d_camera));
+    checkCudaErrors(cudaFree(d_world));
+    checkCudaErrors(cudaFree(d_list));
+    checkCudaErrors(cudaFree(d_rand_state));
+    checkCudaErrors(cudaFree(d_rand_state2));
+    checkCudaErrors(cudaFree(frame_buffer));
+
+    cudaDeviceReset();
+}
+
+void benchmark_tiled(int image_height, int image_width, int samples_per_pixel, int num_frames_to_render) {
+    std::cerr << "Not implemented." << std::endl;
+    exit(1);
+}
+
+void benchmark_frame(int image_height, int image_width, int samples_per_pixel, int num_frames_to_render) {
+    std::cerr << "Not implemented." << std::endl;
+    exit(1);
+}
+
+// Benchmarks the throughput of a rendering type.
+void benchmark_rendering(std::string rendering_strategy, int image_height, int image_width, int samples_per_pixel, int num_frames_to_render) {
+    if (strcmp(rendering_strategy.c_str(), "singlenode") == 0) {
+        benchmark_single(image_height, image_width, samples_per_pixel, num_frames_to_render);
+    } else if (strcmp(rendering_strategy.c_str(), "tiled") == 0) {
+        benchmark_tiled(image_height, image_width, samples_per_pixel, num_frames_to_render);
+    } else if (strcmp(rendering_strategy.c_str(), "frame") == 0) {
+        benchmark_frame(image_height, image_width, samples_per_pixel, num_frames_to_render);
+    }
+}
+
+int main(int argc, char **argv) {
+    // Parse Args
+    if (find_arg_idx(argc, argv, "-h") >= 0) {
+        std::cout << "Options:" << std::endl;
+        std::cout << "-h: see this help" << std::endl;
+        std::cout << "-t <int>: type, 0 = test, 1 = benchmark" << std::endl;
+        std::cout << "-r <rendering strategy>: singlenode/tiled/frame" << std::endl;
+        std::cout << "-v <int>: vertical height of image in pixels" << std::endl;
+        std::cout << "-w <int>: width of image in pixels" << std::endl;
+        std::cout << "-s <int>: number of samples per pixel" << std::endl;
+        std::cout << "-f <int>: number of frames to render" << std::endl;
+        return 0;
+    }
+
+    int image_height = find_int_arg(argc, argv, "-v", 800);
+    int image_width = find_int_arg(argc, argv, "-w", 1200);
+    int samples_per_pixel = find_int_arg(argc, argv, "-s", 10);
+
+    int type = find_int_arg(argc, argv, "-t", 0);
+    if (type == 0) {
+        test_render(image_height, image_width, samples_per_pixel);
+        return 0;
+    }
+
+    int num_frames_to_render = find_int_arg(argc, argv, "-f", 30);
+
+    std::string rendering_strategy = find_string_option(argc, argv, "-r", std::string("singlenode"));
+    if (strcmp(rendering_strategy.c_str(), "singlenode") != 0  && strcmp(rendering_strategy.c_str(), "tiled") != 0 && strcmp(rendering_strategy.c_str(), "frame") != 0) {
+        std::cerr << "Unknown rendering strategy: " << rendering_strategy << std::endl;
+        return 1;
+    }
+
+    benchmark_rendering(rendering_strategy, image_height, image_width, samples_per_pixel, num_frames_to_render);
 }
